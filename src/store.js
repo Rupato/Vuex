1e65f627b87e68576505f74259de0de57e1d8a7a
rupato20 <rupatob15@gmail.com>
2019-11-24 20:23:03 +0530

    first commit

--- src/store.js
+++ src/store.js
@@ -5,12 +5,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'My custom title',
+    //safe an array
+    links: [
+      'https://www.google.co.in',
+      'https://www.facebook.com',
+      'https://www.youtube.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
-
+    //payload to send data to add new link 
+      ADD_LINK: (state, link) => {
+        state.links.push(link)
+      },
+      REMOVE_LINK:(state, link) => {
+        state.links.splice(link)
+      },
+      REMOVE_ALL:(state) => {
+        state.links = []
+      } 
   },
   actions: {
-
-  }
+      removeLink : (context, link) => {
+        context.commit("REMOVE_LINK", link)
+      },
+      removeAll ({commit}) {                    
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            commit('REMOVE_ALL')
+            resolve()
+          }, 1500)
+        })
+      }
+    }
 })
